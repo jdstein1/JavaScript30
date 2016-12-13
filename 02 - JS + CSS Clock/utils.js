1d8fe06762cbb9dd65cd6688ba50631e08b84f79
Stein, Jeff <jstein0@searshc.com>
2016-12-13 16:43:28 -0600

    work on countdown.  improve digital clock.

---
+++ 02 - JS + CSS Clock/utils.js
@@ -0,0 +1,28 @@
+/*utils.js*/
+
+const bold = 900;
+
+const days = {};
+days.long = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+days.short = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+days.number = ['01','02','03','04','05','06','07'];
+console.log('days: ', days);
+
+const months = {};
+months.long = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+months.short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+months.number = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+console.log('months: ', months);
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+const time = document.querySelectorAll(".time");
+time.forEach(function(e){
+  console.log('e: ', e);
+  e.innerHTML = "--"
+});
+
