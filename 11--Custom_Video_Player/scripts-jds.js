7f2fe1745d6824f4a25f93cd84a5aa87bf2c984a
Jeff S <jstein0@searshc.com>
2017-01-03 15:32:21 -0600

    make scrubbing work...mostly.

--- 11--Custom_Video_Player/scripts-jds.js
+++ 11--Custom_Video_Player/scripts-jds.js
@@ -16,7 +16,7 @@ const progressBar = controls.querySelector('.progress__filled');
 // console.log(progressBar);
 
 const toggle = controls.querySelector('.toggle');
-console.log(toggle);
+// console.log(toggle);
 const skipButtons = controls.querySelectorAll('[data-skip]');
 // console.log(skipButtons);
 const ranges = controls.querySelectorAll('[type="range"]');
@@ -69,30 +69,31 @@ play triangle
 }
 
 function skip() {
-  console.log('SKIPPING!!',this.dataset.skip);
+  // console.log('SKIPPING!!',this.dataset.skip);
   const skipVal = parseFloat(this.dataset.skip);
-  console.log('old currentTime: ',video.currentTime);
+  // console.log('old currentTime: ',video.currentTime);
   if (video.played) {
     video.currentTime += (skipVal);
   }
-  console.log('new currentTime: ',video.currentTime);
+  // console.log('new currentTime: ',video.currentTime);
 }
 
 function updateRange() {
-  console.log('updateRange: ', this.name,'value: '+this.value);
-  console.log('old video['+this.name+'] =', video[this.name]);
+  // console.log(this);
+  // console.log('updateRange: ', this.name,'value: '+this.value);
+  // console.log('old video['+this.name+'] =', video[this.name]);
   video[this.name] = this.value;
-  console.log('new video['+this.name+'] =', video[this.name]);
+  // console.log('new video['+this.name+'] =', video[this.name]);
 }
 
 function autoProgress() {
   if (!isSkipping) {
-    // console.group('START autoProgress');
+    console.group('START autoProgress');
     // console.log('autoProgress -- video.duration', video.duration);
     console.log('autoProgress -- old progressBar: ', progressBar.style.flexBasis);
     progressBar.style.flexBasis = (video.currentTime / video.duration)*100+'%';
     console.log('autoProgress -- new progressBar: ', progressBar.style.flexBasis);
-    // console.groupEnd();
+    console.groupEnd();
   }
 }
 
@@ -100,26 +101,39 @@ window.setInterval(autoProgress, 100);
 
 let isSkipping = false;
 
-/*function manualProgress(e) {
+function manualProgress(e) {
+
+  /* Stop propagation */
+  e = e || window.event // cross-browser event
+  if (e.stopPropagation) {
+      // W3C standard variant
+      e.stopPropagation()
+  } else {
+      // IE variant
+      e.cancelBubble = true
+  }
+
+    console.group('START manualProgress: ', isSkipping);
+    console.log('manualProgress -- e.target: ', e.target.classList[0]);
+    console.log('manualProgress -- e.offsetX: ', e.offsetX);
+    console.log('manualProgress -- progress.offsetWidth: ', progress.offsetWidth);
+    console.log('manualProgress -- position: ', (e.offsetX / progress.offsetWidth)*100+'%');
   if (isSkipping) {
-    // console.group('START manualProgress: ', isSkipping);
     // console.log('manualProgress -- e.type: ', e.type);
-    console.log('manualProgress -- e.target: ', e.target);
-    console.log('manualProgress -- progressBar: ', progressBar);
+    // console.log('manualProgress -- e.target: ', e.target.classList[0]);
+    // console.log('manualProgress -- progressBar: ', progressBar.classList[0]);
     // console.log('manualProgress -- e.target.style.flexBasis: ', e.target.style.flexBasis);
-    // console.log('manualProgress -- e.target.offsetWidth: ', e.target.offsetWidth);
-    // console.log('manualProgress -- e.offsetX: ', e.offsetX);
     // console.log('manualProgress -- old manualProgress: ', progressBar.style.flexBasis);
-    console.log('manualProgress -- old video.currentTime: ',video.currentTime);
-    video.currentTime = video.duration * (e.offsetX / e.target.offsetWidth);
-    // progressBar.style.flexBasis = (e.offsetX / e.target.offsetWidth)*100+'%';
-    console.log('manualProgress -- new video.currentTime: ',video.currentTime);
+    // console.log('manualProgress -- old video.currentTime: ',video.currentTime);
+    video.currentTime = video.duration * (e.offsetX / progress.offsetWidth);
+    progressBar.style.flexBasis = (e.offsetX / progress.offsetWidth)*100+'%';
+    // console.log('manualProgress -- new video.currentTime: ',video.currentTime);
     // console.log('manualProgress -- progressBar.style.flexBasis: ',progressBar.style.flexBasis);
     // autoProgress;
     // console.log('manualProgress -- new manualProgress: ', progressBar.style.flexBasis);
-    // console.groupEnd();
   }
-}*/
+    console.groupEnd();
+}
 
 /**
  * hook up event listeners
@@ -131,7 +145,10 @@ toggle.addEventListener('click',togglePlayer);
 skipButtons.forEach(button => button.addEventListener('click',skip));
 ranges.forEach(range => range.addEventListener('change',updateRange));
 
-// progress.addEventListener('mousedown', () => isSkipping = true);
-// progress.addEventListener('mousemove',manualProgress);
-// progress.addEventListener('mouseup', () => isSkipping = false);
-// progress.addEventListener('mouseout', () => isSkipping = false);
+progress.addEventListener('mousedown', () => {
+  isSkipping = true;
+  manualProgress();
+});
+progress.addEventListener('mousemove', manualProgress);
+progress.addEventListener('mouseup', () => isSkipping = false);
+progress.addEventListener('mouseout', () => isSkipping = false);
