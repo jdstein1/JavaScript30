f5ca6529aa64689209c1bae620e4711d3d8fe5b2
Jeff S <jstein0@searshc.com>
2017-01-04 11:45:09 -0600

    optimize pros 11 scripts.

--- 11--Custom_Video_Player/scripts-jds.js
+++ 11--Custom_Video_Player/scripts-jds.js
@@ -2,26 +2,13 @@
  * get elements
  */
 const player = document.querySelector('.player');
-// console.log('player: ', player);
-
 const video = player.querySelector('.viewer');
-// console.log('video: ', video);
-
 const controls = player.querySelector('.player__controls');
-// console.log('controls: ', controls);
-
 const progress = controls.querySelector('.progress');
-// console.log(progress);
 const progressBar = controls.querySelector('.progress__filled');
-// console.log(progressBar);
-
 const toggle = controls.querySelector('.toggle');
-// console.log(toggle);
 const skipButtons = controls.querySelectorAll('[data-skip]');
-// console.log(skipButtons);
 const ranges = controls.querySelectorAll('[type="range"]');
-// console.log(ranges);
-
 const fullScreen = controls.querySelector('[name="fullScreen"]');
 
 /**
@@ -88,16 +75,16 @@ function updateRange() {
   // console.log('new video['+this.name+'] =', video[this.name]);
 }
 
-function autoProgress() {
+function fill() {
   if (!isScrubbing) {
-    console.group('START autoProgress -- isScrubbing: ', isScrubbing);
-    console.log('autoProgress -- old currentTime: ', video.currentTime);
+    // console.group('START fill -- isScrubbing: ', isScrubbing);
+    // console.log('fill -- old currentTime: ', video.currentTime);
     // progressBar.style.flexBasis = (video.currentTime / video.duration)*100+'%';
     const percent = (video.currentTime / video.duration)*100;
     // console.log(percent);
     progressBar.style.flexBasis = `${percent}%`;
-    console.log('autoProgress -- new currentTime: ', video.currentTime);
-    console.groupEnd();
+    // console.log('fill -- new currentTime: ', video.currentTime);
+    // console.groupEnd();
   }
 }
 
@@ -113,18 +100,18 @@ function scrub(e) {
       // IE variant
       e.cancelBubble = true
   }
-  console.group('START scrub -- isScrubbing: ', isScrubbing);
-  console.log('scrub -- old currentTime: ',video.currentTime);
+  // console.group('START scrub -- isScrubbing: ', isScrubbing);
+  // console.log('scrub -- old currentTime: ',video.currentTime);
   video.currentTime = video.duration * (e.offsetX / progress.offsetWidth);
   progressBar.style.flexBasis = (video.currentTime / video.duration)*100+'%';
-  console.log('scrub -- new currentTime: ',video.currentTime);
-  console.groupEnd();
+  // console.log('scrub -- new currentTime: ',video.currentTime);
+  // console.groupEnd();
 }
 
 function goFullScreen(e) {
-  console.log('START goFullScreen');
+  // console.log('START goFullScreen');
   if (video.requestFullscreen) {
-    console.log('requestFullscreen');
+    // console.log('requestFullscreen');
     video.requestFullscreen();
   }
 }
@@ -135,7 +122,7 @@ function goFullScreen(e) {
 video.addEventListener('click',togglePlayer);
 video.addEventListener('play',toggleButton);
 video.addEventListener('pause',toggleButton);
-video.addEventListener('timeupdate',autoProgress);
+video.addEventListener('timeupdate',fill);
 
 toggle.addEventListener('click',togglePlayer);
 
