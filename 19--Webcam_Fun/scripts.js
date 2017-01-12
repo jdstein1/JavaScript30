7460235a909d7fc889e464a9fa4d19e24f1cf9a6
Jeff S <jdstein1@gmail.com>
2017-01-12 16:01:15 -0600

    Project 19: make video print image to canvas.

--- 19--Webcam_Fun/scripts.js
+++ 19--Webcam_Fun/scripts.js
@@ -1,71 +1,115 @@
 
 /* DOM NODES */
 const video = document.querySelector('.video');
-console.log('video: ', video);
-
 const canvas = document.querySelector('.canvas');
-console.log('canvas: ', canvas);
-
 const ctx = canvas.getContext('2d');
-console.log('ctx: ', ctx);
-
 const strip = document.querySelector('.strip');
-console.log('strip: ', strip);
-
 const snap = document.querySelector('.snap');
-console.log('snap: ', snap);
 
+/* VARIABLES */
+let myStream;
+let front = true;
+let positionX = 0;
+let positionY = 0;
 
 /* FUNCTIONS */
-const myConstraints = {audio: true, video: {
+const videoSettings = {audio: true, video: true};
+  // {
     // width: { min: 1024, ideal: 1280, max: 1920 },
     // height: { min: 776, ideal: 720, max: 1080 },
-    facingMode: "user"
-  }};
+    // facingMode: (front ? "user" : "environment")
+  // }
 
-function getVideo() {
+function getVideo(mode) {
   console.log('START getVideo');
-  navigator.mediaDevices.getUserMedia(myConstraints)
+  navigator.mediaDevices.getUserMedia(videoSettings)
   .then(function(mediaStream) {
-    /* use the stream */
-    console.log('mediaStream: ', mediaStream);
-    /* method 1 */
-    video.src = window.URL.createObjectURL(mediaStream);
-    video.play();
-    /* method 2 */
-    // video.srcObject = mediaStream;
-    // video.onloadedmetadata = function(e) {
-    //   video.play();
-    // };
+    myStream = mediaStream;
+    if (mode && mode === 'start') {
+      /* method 1 */
+      video.src = window.URL.createObjectURL(myStream);
+      // video.play();
+      /* method 2 */
+      // video.srcObject = myStream;
+      video.onloadedmetadata = function(e) {
+        video.play();
+        videoToCanvas();
+      };
+    } else {
+      video.src = '';
+      // video.pause();
+    }
+    console.log('myStream: ', myStream);
   })
   .catch(function(err) {
-    /* handle the error */
     console.error('err: ', err);
     // console.log(err.name + ": " + err.message); // always check for errors at the end.
   });
+  // console.groupEnd();
 }
-// getVideo();
 
 function videoToCanvas() {
-  console.log('START videoToCanvas');
-  const w = video.videoWidth;
-  const h = video.videoHeight;
-  console.log(w,h);
+  console.group('START videoToCanvas');
+  console.log(video);
+
+  const vWidth = video.videoWidth;
+  const vHeight = video.videoHeight;
+  console.log(vWidth,vHeight);
+
+  const wWidth = window.innerWidth;
+  const wHeight = window.innerHeight;
+  console.log(wWidth,wHeight);
+
+  canvas.width = vWidth;
+  canvas.height = vHeight;
+
+  // positionX = (wWidth - vWidth)/2;
+  // positionY = (wHeight - vHeight)/2;
+
+  if (vWidth < wWidth && vHeight > wHeight) {
+    console.log('video taller than window');
+    // stretch video vertically...
+    // canvas.width = vWidth;
+    // canvas.height = wHeight;
+  } else if (vWidth > wWidth && vHeight < wHeight) {
+    console.log('video wider than window');
+    // stretch video horizontally...
+    // canvas.width = wWidth;
+    // canvas.height = vHeight;
+  } else {
+    console.log('video ??? than window');
+    // canvas.width = vWidth;
+    // canvas.height = vHeight;
+  }
+
+  // console.log(canvas.width,canvas.height);
+  setInterval(() => {
+    ctx.drawImage(video,positionX,positionY,vWidth,vHeight);
+  }, 100);
+  console.groupEnd();
 }
-videoToCanvas();
+
+function flipCamera() {
+  front = !front;
+  alert('I JUST FLIPPED YOUR CAMERA');
+}
 
 function takePhoto() {
   alert('I JUST TOOK YOUR PHOTO');
 }
 
 function startStream() {
-  mediaStream.active = true;
+  console.group('CAMERA ON');
+  // myStream.active = true;
+  getVideo('start');
+  console.groupEnd();
 }
 
 function stopStream() {
-  mediaStream.active = false;
+  console.group('CAMERA OFF');
+  // myStream.active = false;
+  getVideo('stop');
+  console.groupEnd();
 }
 
-
 /* EVENT LISTENERS */
-
